1a4dc8517f3043345b7f062bd581d52271a9b887
mohagungnursalim <mohagungnursalim@gmail.com>
2024-02-29 09:34:30 +0800

    menghilangkan script preview image pada halaman komoditas/index

--- resources/views/dashboard/komoditas/index.blade.php
+++ resources/views/dashboard/komoditas/index.blade.php
@@ -209,25 +209,3 @@ Komoditas
 @endif
 
 
-{{-- input image preciew --}}
-<script>
-    function previewImage() {
-
-        const image = document.querySelector('#image');
-        const imgPreview = document.querySelector('.img-preview');
-
-        imgPreview.style.display = 'block';
-
-        const oFReader = new FileReader();
-
-        oFReader.readAsDataURL(image.files[0]);
-
-        oFReader.onload = function (oFREvent) {
-
-            imgPreview.src = oFREvent.target.result;
-
-        }
-    }
-
-</script>
-
